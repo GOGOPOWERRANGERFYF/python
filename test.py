6aad0c8f30d03388e1625335423335cd2dd07532
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-16 01:08:45 +0800

    test

---
+++ test.py
@@ -0,0 +1,14 @@
+exit_fyf = False
+a = ''
+i = input('input digit:')
+
+while i != 'exit':
+    try:
+        a = float(i)
+    except:
+        a = '要输入数字啊！英文看不懂吗？'
+    print(a)
+    i =input('input digit again:')
+print ('不玩滚！')
+
+
