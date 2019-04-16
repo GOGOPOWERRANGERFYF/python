6921fc1e300c604fef1458ed512e566201744d71
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-16 18:49:06 +0800

    class_example

--- class_example/class_students.py
+++ class_example/class_students.py
@@ -1,3 +1,9 @@
+'''
+object oriented progamming 面向对象编程，是一种程序设计思想
+在python中，所有数据类型都可以视为对象，当然也可以自定义对象，自定义的数据类型就是面向
+对象中的类(class)
+'''
+# int   float   string  boolean这些数据类型并不能表示一个学生，因此自定义一个学生数据类型，也就类class
 class Students:
     def __init__(self,name,age,score):
         self.name = name
--- class_example/students.py
+++ class_example/students.py
@@ -1,3 +1,7 @@
 from class_students import Students    #从class_students文件导入Students类(class)
 
-students1 = Students('fyf',18,100)
+#object is  an instance on the class 对象object是一个类class的实例instance
+student1 = Students('fyf',18,100) #这个类就像一个模板它定义了一个学生是什么,实例化一个对象
+
+print(student1)
+print(student1.name)
