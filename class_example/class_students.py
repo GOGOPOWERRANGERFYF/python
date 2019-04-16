9d495fbfa3e71fc1b417a7431a8e12b140cfc1b7
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-16 17:50:53 +0800

    class_example

---
+++ class_example/class_students.py
@@ -0,0 +1,5 @@
+class Students:
+    def __init__(self,name,age,score):
+        self.name = name
+        self.age = age
+        self.score = score
---
+++ class_example/students.py
@@ -0,0 +1,3 @@
+from class_students import Students    #从class_students文件导入Students类(class)
+
+students1 = Students('fyf',18,100)
