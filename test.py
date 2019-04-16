4e2b3e1e73229049bb3f426422f349eba601b830
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-16 16:12:17 +0800

    test

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
-i = input('input digit:')
+def input_digit_game():
+    i = input('input digit:')
+    while i != 'exit':
+        try:
+            a = float(i)
+        except:
+            a = '要输入数字啊！英文看不懂吗？'
+        print(a)
+        i =input('input digit again:')
+    print ('不玩滚！')
+    
 
-while i != 'exit':
-    try:
-        a = float(i)
-    except:
-        a = '要输入数字啊！英文看不懂吗？'
-    print(a)
-    i =input('input digit again:')
-print ('不玩滚！')
-
+input_digit_game()
 
