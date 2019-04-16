570475e91149925cad3d329de6332673df20aa20
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-16 15:51:05 +0800

    decorator

--- decorator.py
+++ decorator.py
@@ -33,12 +33,12 @@ print(make_cake())
 '''
 
 # 3.
-def add_candle(cake):
-    def insert_candle():
-        return cake() + " candle"
+def add_candle(cake):              #cake为函数名function
+    def insert_candle():          #内部函数调用外部函数的参数，这个参数是一个函数，所以是特殊的闭包函数
+        return cake() + " candle"  #cake()函数调用call
     return insert_candle
 
-@add_candle
+@add_candle                       #make_cake函数作为参数传入add_candle函数中
 def make_cake():
     return "cake"
 
