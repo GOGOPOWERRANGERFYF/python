4a1d977467f1404ce11b481cb46d9c284af2b62e
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-17 18:04:35 +0800

    class

--- class_example/students.py
+++ class_example/students.py
@@ -1,8 +1,9 @@
 from class_students import Students    #从class_students文件导入Students类(class)
 
 #object is  an instance on the class 对象object是一个类class的实例instance
-student1 = Students('fyf',18,100) #这个类就像一个模板它定义了一个学生是什么,实例化一个对象
-student2 = Students('fxf',20,75) 
+student1 = Students('fyf',22,100) #这个类就像一个模板它定义了一个学生是什么,实例化一个对象
+student2 = Students('haha',19,75) 
+student3 = Students('sb',20,75) 
 
 print(student1)
 print(student1.name)
@@ -11,5 +12,6 @@ print(student1.score)
 
 student1.level()
 student2.level()
+student3.level()
 
 
