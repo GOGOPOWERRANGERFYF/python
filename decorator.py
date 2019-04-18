8417a1da11c864d4211b0815de25a4816cb9f2c2
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-19 00:56:21 +0800

    modify

--- decorator.py
+++ decorator.py
@@ -31,8 +31,8 @@ make_cake = add_candle(make_cake)
 print(make_cake.__name__)
 print(make_cake())
 '''
-
-# 3.
+'''
+3.
 def add_candle(cake):              #cake为函数名function
     def insert_candle():          #内部函数调用外部函数的参数，这个参数是一个函数，所以是特殊的闭包函数
         return cake() + " candle"  #cake()函数调用call
@@ -44,3 +44,46 @@ def make_cake():
 
 print(make_cake.__name__)
 print(make_cake())
+'''
+'''
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+'''
+'''
+带参数的装饰器decorator
+'''
+
+#def cake_name(x): #装饰器外再套一层函数,x参数可以传入闭包函数内
+#    def add_candle(f):  
+#        def insert_candle():
+#            print('%s ' %x + '%s add candle :)' %f())
+#        return insert_candle
+#    return add_candle #x传入后,中间为定义闭包函数enclosing function,而且是装饰器decorator,这一步返回其实就是返回decorator
+#
+##作用：把数据x封装起来，x是全局变量的话修改后也会影响其它调用该变量的函数
+#@cake_name('Yes, This is alibaba') #传入参数x,返回add_candle，等于@add_candle = add_candle(make_cake)
+#def make_cake():                   
+#    return 'cake'
+#
+#make_cake()
+
+
+
+def cake_name(x): #装饰器外再套一层函数,x参数可以传入闭包函数内
+    def add_candle(f):  
+        def insert_candle():
+            print(x)
+            print(f(x))
+        return insert_candle
+    return add_candle #(1)x传入后,中间为定义闭包函数enclosing function,而且是装饰器decorator,这一步返回其实就是返回decorator
+
+#作用：把数据x封装起来，x是全局变量的话修改后也会影响其它调用该变量的函数
+@cake_name('Yes, This is alibaba') #(2)传入参数x,返回add_candle，等于@add_candle = add_candle(make_cake)=make_cake()
+def make_cake(x):                   
+    return '被装饰函数接收到了带参数的装饰器的传入参数：%s' %x
+
+make_cake()
+
+
+
+
+
--- flask_fyf.py
+++ flask_fyf.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  #带参数的装饰器
 def index():
     return render_template('index.html')
 
