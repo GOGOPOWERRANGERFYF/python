4fd21e357e429b0cfb6ab2d1d512991efd7a0fed
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-04-21 00:52:55 +0800

    modify

--- flask_fyf.py
+++ flask_fyf.py
@@ -8,13 +8,14 @@ def index():
     #    name = 'sss'
     #    age = 18
     #s = student
+    list1 = ['fyf',22,True]
     context = {                                   #定义一个字典
         'name' : '诸葛亮',                           #key-value键值对,key必须为string字符串
         'age' : 100,
         'profile' : '诸葛亮散文代表作有《出师表》《诫子书》等'
     }
     date = '上午 10:35'
-    return render_template('index.html',context=context,date=date)#**关键字参数，传入参数后自动封装成一个字典dictionary，*关键字参数，传入元组tuple，传入参数后前端直接调用就行了，框架已经帮处理好了，不用担心数据类型的问题
+    return render_template('index.html',context=context,date=date,list1=list1)#**关键字参数，传入参数后自动封装成一个字典dictionary，*关键字参数，传入元组tuple，传入参数后前端直接调用就行了，框架已经帮处理好了，不用担心数据类型的问题I
 
 @app.route('/article/<id>')       #带参数的装饰器，URL的一部分标记为<variable_name>就可以在
 def url_id(id):                   #URL中添加变量,标记的部分会作为关键字参数传递给函数
@@ -36,7 +37,6 @@ def web_login(fyf_login):
         return render_template('login.html',user=user)
     else:
         return render_template('login.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
