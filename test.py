f2f2c4412275a209d18b9330f5e2ec7ab4a45cd0
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-05-02 11:59:44 +0800

    modify

--- test.py
+++ test.py
@@ -200,15 +200,15 @@
 
 
 class screen(object):
-    #def __init__(self,width,height):
-    #    self.__width=width            #__width相当于私有变量，private，外部不能直接调用call 
-    #    self.__height=height
+    def __init__(self,width,height):
+        self.__width=width   #__width相当于私有变量，private，外部不能直接调用call 
+        self.__height=height #___双下划线：修饰变量名，可通过dir()内置函数（built-in function）查看属性时看到
     
-    @property                         #@property装饰器，把对象的方法装饰成属性，外部直接像调用属性一样调用即可，不用call（就是不用加 ( )）
+    @property                #@property装饰器，把对象的方法装饰成属性，外部直接像调用属性一样调用即可，不用call（就是不用加 ( )），相当于定义变量
     def width(self):
         return self.__width
 
-    @width.setter                     
+    @width.setter            #把函数装饰成属性，@width.setter装饰器由上头这段代码的@property装饰器生成，如果不定义，相当于只读属性，加上这段可以像普通变量一样赋值
     def width(self,value):
         self.__width = value
 
