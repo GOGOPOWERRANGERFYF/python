a0fe70406a643e0773b829604de7a6fd7ce2fecd
GOGOPOWERRANGERFYF <392138779@qq.com>
2019-05-06 14:03:24 +0800

    modify

--- flask_fyf.py
+++ flask_fyf.py
@@ -35,6 +35,12 @@ class User(db.Model):                    # 看源码继承的是 class Model(arg
     email = db.Column(db.String(32),unique=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))   #ForeignKey外键
     
+    '''
+    u = User()  #transient声明一个实例变量
+    u.query.all()    #query.all  u对象object的捆绑方法bound method
+    终端返回：[<User 1>, <User 2>, <User 3>]   #一个列表list，里面是User数据模型(类class)的对象,所以通过自定义python内建方法__repr__返回想要得到的字符串
+    '''
+
     #下面两个内建特殊方法 __str__   __repr__   也是python内建函数str()   repr() 
     # 作用：object对象的字符串显示    
     #默认情况下，__str__ 等于__repr__ ，    当只定义__repr__时，__str__等于__repr__ ;     当之定义__str__时，两者不相等，通过交互命令行模式验证
@@ -42,7 +48,9 @@ class User(db.Model):                    # 看源码继承的是 class Model(arg
     #    return 'users: %d %s %s %s' %(self.id,self.name,self.email,self.password)
     #作用：自定义User()对象的字符串显示       不自定义的话，默认的__repr__从父类base class继承，直至object类
     def __repr__(self):    #python 内建built-in function函数       representation      repr(object)     object.__repr__( )     python内建的特殊方法    
-        return 'users: %d %s %s %s' %(self.id,self.name,self.email,self.password)
+        return '%s %s %s' %(self.name,self.email,self.password)
+        #return 'users: %s %s %s' %(self.name,self.email,self.password)
+        #return 'users: %d %s %s %s' %(self.id,self.name,self.email,self.password)
 
 
 '''
